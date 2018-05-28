3f7c651006487da1044917a695da92ed4329c9c2
takechi03 <takechi_iman@hotmail.com>
2018-05-28 11:04:27 +0700

    Fix system form javascript

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var window_width_video = $(window).width(),
+    window_height_video = $(window).height(),
+    flagwrapper = $(".flag-wrapper").width(),
+    heightpicture,
+    data = "translateY(calc(62% + 5em))";
+
+heightpicture = window_height_video / 2.40 - 30;
+$(".map-kingpower").css("height", window_height_video);
+$(".map-kingpower").css("width", window_width_video);
+
+$(".map-kingpower-page-1").css("height", window_height_video - 50);
+$(".map-kingpower-page-1").css("width", window_width_video);
+
+$(".map-kingpower div.wrapper").css({
+    "min-height": heightpicture
+});
+$(".map-kingpower-page-1 div.wrapper").css({
+    "min-height": window_height_video 
+});
+
+
+$(".card .content").css({
+    "height": heightpicture
+});
+
+$(".flag-wrapper").css({
+    "height": flagwrapper,
+    "width": flagwrapper
+});
+if (window_width_video > 768) {
+    $(".map-kingpower,.map-kingpower-page-1").css("overflow", "hidden");
+}
+
+console.log(window_height_video + " : " + window_width_video + " : MIN-HEIGHT = " + heightpicture);
