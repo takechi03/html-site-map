fefa546761b8ba3318e105127aec31523da61ee7
takechi03 <takechi_iman@hotmail.com>
2018-06-01 18:01:38 +0700

    fix javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var window_width_video = $(window).width(),
     window_height_video = $(window).height(),
     flagwrapper = $(".flag-wrapper").width(),
+    language = $(".language").height(),
     heightpicture,
     data = "translateY(calc(62% + 5em))";
 
 heightpicture = window_height_video / 2.40 - 30;
+paddingforcard2 = heightpicture * 0.05;
+
+console.log("padding " + paddingforcard2 + "height picture : " + heightpicture + " | Flagwrapper : " + flagwrapper + " | language height : " + language);
 $(".map-kingpower").css("height", window_height_video);
 $(".map-kingpower").css("width", window_width_video);
 
@@ -14,20 +18,28 @@ $(".map-kingpower-page-1").css("width", window_width_video);
 $(".map-kingpower div.wrapper").css({
     "min-height": heightpicture
 });
-$(".map-kingpower-page-1 div.wrapper").css({
-    "min-height": window_height_video 
-});
+// $(".map-kingpower-page-1 div.wrapper").css({
+//     "min-height": window_height_video 
+// });
 
+$(".map-kingpower-page-1 .language").css({
+    "padding-top" : language,
+    "padding-bottom" : language,
+    "padding-left" : language/2,
+    "padding-right" : language/2
+});
 
 $(".card .content").css({
-    "height": heightpicture
+    //"height": heightpicture
+    "padding":paddingforcard2,
 });
 
-$(".map-kingpower-page-1 .card .content").css({
-    "height": window_height_video
+$(".card2 .content").css({
+    "padding":paddingforcard2,
+    //"height": heightpicture
 });
 
-$(".flag-wrapper").css({
+$(".map-kingpower .flag-wrapper").css({
     "height": flagwrapper,
     "width": flagwrapper
 });
